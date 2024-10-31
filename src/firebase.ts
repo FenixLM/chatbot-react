2977401badb7834d3505309bcfe2514368ccebe3
Felix Lamadrid <felix.lamadrid97@gmail.com>
2024-10-31 02:53:58 -0500

    integracion de firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCwA0O3Ki2-FW9r1_SohcXeg7UkObX3CuE',
-  authDomain: 'chat-ia-vectores.firebaseapp.com',
-  projectId: 'chat-ia-vectores',
-  storageBucket: 'chat-ia-vectores.appspot.com',
-  messagingSenderId: '789009233563',
-  appId: '1:789009233563:web:fd4b7e603d6743a3d1f83e',
-  measurementId: 'G-4XPS7XJYVW',
+  apiKey: '### FIREBASE API KEY ###',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: '### FIREBASE PROJECT ID ###',
+  storageBucket: '### FIREBASE STORAGE BUCKET ###',
+  messagingSenderId: '### FIREBASE MESSAGING SENDER ID ###',
+  appId: '### FIREBASE APP ID ###',
+  measurementId: '### FIREBASE MEASUREMENT ID ###',
 };
 
 const app = initializeApp(firebaseConfig);
