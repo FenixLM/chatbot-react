59f59f4afdec34a3e3143af60b69dce2d0fdc9ec
Felix Lamadrid <felix.lamadrid97@gmail.com>
2024-10-31 03:01:59 -0500

    Integration example

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: '### FIREBASE API KEY ###',
-  authDomain: '### FIREBASE AUTH DOMAIN ###',
-  projectId: '### FIREBASE PROJECT ID ###',
-  storageBucket: '### FIREBASE STORAGE BUCKET ###',
-  messagingSenderId: '### FIREBASE MESSAGING SENDER ID ###',
-  appId: '### FIREBASE APP ID ###',
-  measurementId: '### FIREBASE MEASUREMENT ID ###',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
